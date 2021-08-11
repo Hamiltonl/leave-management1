e36f4bef8f8765be263f1ee236f48f12dc5746b4
Hamilton Luz <Hamilton Luz@WKS-1629>
2021-08-11 15:22:56 +0100

    Add project files.

---
+++ leave-management1/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using leave_management1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management1.Contracts
+{
+    public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+        ICollection<LeaveType> GetEmployeesByLeaveType(int id);
+    }
+}
---
+++ leave-management1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace leave_management1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
